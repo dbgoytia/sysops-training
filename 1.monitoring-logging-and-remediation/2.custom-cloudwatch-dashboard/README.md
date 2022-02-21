d72e3609decaff88bf72d366012d01d506d100dd
Diego Goytia <diego.canizales1995@gmail.com>
2022-02-21 11:54:16 -0600

    readme.

---
+++ 1.monitoring-logging-and-remediation/2.custom-cloudwatch-dashboard/README.md
@@ -0,0 +1,72 @@
+## Goal
+
+Goal of this demonstration is to understand how to deploy a custom CloudWatch dashboard
+that utilizes some widgets to help us get a better view of our environments. You'll also
+have the chance to use AWS SSM to log in to the instances.
+
+The metrics we're going to see are:
+* CPUUtilization
+* NetworkPacketsIn
+* NetworkPacketsOut
+
+We're going to launch an EC2 instance, generate some activity, and capture the metrics
+listed above to see the usage.
+
+
+## Final result:
+
+![EC2 Dashboard](https://github.com/dbgoytia/sysops-training/blob/main/1.monitoring-logging-and-remediation/2.custom-cloudwatch-dashboard/dashboard.png?raw=true)
+
+
+## Objective
+
+1. Launch the terraform code in here.
+
+    ```
+    # Initialize terraform providers
+    $ terraform init
+    ```
+
+    ```
+    # Create a plan for the lab, validate everything's ok.
+    $ terraform plan -out tfplan 
+    ```
+
+    ```
+    # Launch terraform
+    $ terraform apply tfplan
+    ```
+
+2. Connect to the instance and generate some random activity. Let this run for about 10 seconds.
+
+    ```
+    # Log In to the EC2 Instance
+    $ aws ssm start-session --target <your-instance-id> --region us-east-1
+
+    # Will generate high spikes in CPU Utilization
+    $ while true; do echo 'hello'; done;
+
+    # Stop the operation usint CTRL+C
+    ```
+
+
+3. What can you see on the 
+
+## To destroy
+
+1. Destroy the terraform code using
+
+```
+# Review what you're about to destroy
+$ terraform plan -out tfplan -destroy
+```
+
+```
+# Destroy the resources
+$ terraform apply tfplan
+```
+
+
+## Cool things I've noted
+
+
---
+++ 1.monitoring-logging-and-remediation/2.custom-cloudwatch-dashboard/lab.tf
@@ -0,0 +1,99 @@
+# Deploy a test VPC
+module "vpc" {
+  source = "git@github.com:dbgoytia/vpc-tf-module.git?ref=v0.0.1"
+
+  region                = "us-east-1"
+  name                  = "test-vpc"
+  azs                   = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  vpc_cidr_block        = "10.255.0.0/17"
+  public_subnets_cidrs  = ["10.255.96.0/21", "10.255.104.0/21", "10.255.112.0/21"] #left over 10.255.120.0/21
+  private_subnets_cidrs = ["10.255.0.0/19", "10.255.32.0/19", "10.255.64.0/19"]
+}
+
+# Launch an ec2 instance
+module "ec2" {
+  source = "git@github.com:dbgoytia/ec2-tf-module.git?ref=v0.0.2"
+
+  ami                         = "ami-02e136e904f3da870"
+  instance_type               = "t2.micro"
+  subnet_id                   = module.vpc.public_subnets_ids[0]
+  az                          = "us-east-1a"
+  associate_public_ip_address = true
+  region                      = "us-east-1"
+  name                        = "test-ec2"
+}
+
+# Create the cloudwatch dashboards
+resource "aws_cloudwatch_dashboard" "instance_utilization_metrics" {
+    dashboard_name = "my-instance-utilization"
+
+    dashboard_body = <<EOF
+{
+  "widgets": [
+    {
+      "type": "metric",
+      "x": 0,
+      "y": 0,
+      "width": 12,
+      "height": 6,
+      "properties": {
+        "metrics": [
+          [
+            "AWS/EC2",
+            "CPUUtilization",
+            "InstanceId",
+            "${module.ec2.instance_id}"
+          ]
+        ],
+        "period": 300,
+        "stat": "Average",
+        "region": "us-east-1",
+        "title": "EC2 Instance CPU"
+      }
+    },
+    {
+      "type": "metric",
+      "x": 0,
+      "y": 0,
+      "width": 12,
+      "height": 6,
+      "properties": {
+        "metrics": [
+          [
+            "AWS/EC2",
+            "NetworkPacketsIn",
+            "InstanceId",
+            "${module.ec2.instance_id}"
+          ]
+        ],
+        "period": 300,
+        "stat": "Average",
+        "region": "us-east-1",
+        "title": "EC2 Network Packets In"
+      }
+    },
+    {
+      "type": "metric",
+      "x": 0,
+      "y": 0,
+      "width": 12,
+      "height": 6,
+      "properties": {
+        "metrics": [
+          [
+            "AWS/EC2",
+            "NetworkPacketsOut",
+            "InstanceId",
+            "${module.ec2.instance_id}"
+          ]
+        ],
+        "period": 300,
+        "stat": "Average",
+        "region": "us-east-1",
+        "title": "EC2 Network Packets Out"
+      }
+    }
+  ]
+}
+EOF
+}
